1388a630bf196a4c9641d3e92b017fb5bd3c1001
XinLi <li.xin.ece@gmail.com>
2019-06-29 19:02:33 -0500

    975 monotonic stack

--- 975-Odd-Even-Jump.cpp
+++ 975-Odd-Even-Jump.cpp
@@ -22,3 +22,51 @@ public:
         return res;
     }
 };
+
+// 试着用单调栈写了一下，直接用单调栈是不行的，还是要先排序
+class Solution {
+public:
+    int oddEvenJumps(vector<int>& A) {
+        int n = A.size();
+        int index[n];
+        int oddNext[n];
+        int evenNext[n];
+        stack<int> st;
+        for ( int i = 0; i < n; ++i ) {
+            index[i] = i;
+        }
+        sort( index, index + n, [&A](int a, int b) {
+            return A[a] == A[b] ? a < b : A[a] < A[b];
+        });
+         for ( int i = n - 1; i >= 0; --i ) {
+            int idx = index[i];
+            while ( !st.empty() && st.top() < idx ) st.pop();
+            oddNext[idx] = st.empty() ? -1 : st.top();
+            st.push( idx );
+        }
+        while ( !st.empty() ) st.pop();
+        sort( index, index + n, [&A](int a, int b) {
+            return A[a] == A[b] ? a < b : A[a] > A[b];
+        });
+        for ( int i = n - 1; i >= 0; --i ) {
+            int idx = index[i];
+            while ( !st.empty() && st.top() < idx ) st.pop();
+            evenNext[idx] = st.empty() ? -1 : st.top();
+            st.push( idx );
+        }
+        bool odd[n];
+        bool even[n];
+        odd[n - 1] = true;
+        even[n - 1] = true;
+        int res = 1;
+        for ( int i = n - 2; i >= 0; --i ) {
+            if ( oddNext[i] == -1 ) odd[i] = false;
+            else odd[i] = even[oddNext[i]];
+            
+            if ( evenNext[i] == -1 ) even[i] = false;
+            else even[i] = odd[evenNext[i]];
+            res += odd[i];
+        }
+        return res;
+    }
+};
